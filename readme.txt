1905a7389f8c1bc48672e8bca8ae88b933a3a8a5
Andrzej Pawlowski <panandy@poczta.onet.pl>
2019-10-01 11:37:14 +0200

    ex from readme.txt done

---
+++ readme.txt
@@ -0,0 +1,3 @@
+Napisz program, który oblicza pole prostokąta. Wartości boków
+a i b wprowadzamy z klawiatury. W programie należy przyjąć,
+że zmienne a, b oraz pole są typu double (rzeczywistego).
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+
+        double a;
+        double b;
+        double pole;
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Podaj długość boku a: ");
+        a = scanner.nextDouble();
+        System.out.println("Podaj długość boku b: ");
+        b = scanner.nextDouble();
+
+        pole = a * b;
+
+        System.out.println("Pole prostokąta wynosi: " + pole);
     }
 }
